ed6f49ed9e063fe6a886df0f32bb09a7ab551be5
SnakeJamzz <k.wittershejm@gmail.com>
2019-03-25 20:19:00 +0100

    dodano kilka funkcji

--- sandbox/src/main/java/qa/practice1/sandbox/Practice1.java
+++ sandbox/src/main/java/qa/practice1/sandbox/Practice1.java
@@ -3,7 +3,29 @@ package qa.practice1.sandbox;
 public class Practice1 {
 
 	public static void main(String[] args) {
-	System.out.println("Giant Steps!");
+	  giant("TURTLE");
+    giant("ANT");
+    giant("HAMSTER");
+
+    double l = 6;
+    System.out.println("powierzchnia kwadratu o boku " + l + " = " + areas(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("powierzchnia prostokąta a bokach " + a + " i " + b + " = " + areas(a, b));
+}
+
+public static void giant(String animal){
+	  System.out.println("Giant " + animal + "!");
+
+}
+public static double areas(double len){
+    return len * len;
+
+}
+
+public static double areas(double a, double b){
+	  return a * b;
 }
 
 }
