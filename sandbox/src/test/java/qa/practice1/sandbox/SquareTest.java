6b0308ff4916c1a97da1007417012db164d39801
SnakeJamzz <k.wittershejm@gmail.com>
2019-03-31 20:56:22 +0200

    Testng issue

--- sandbox/src/test/java/qa/practice1/sandbox/SquareTest.java
+++ sandbox/src/test/java/qa/practice1/sandbox/SquareTest.java
@@ -1,8 +1,10 @@
 package qa.practice1.sandbox;
+import org.testng.annotations.Test;
 
-public class SquareTest {
+public class SquareTest{
 
-    public void testArea(){
+    @Test
+    public void testAreas(){
         Square s = new Square(5);
         assert s.areas() == 25;
     }
