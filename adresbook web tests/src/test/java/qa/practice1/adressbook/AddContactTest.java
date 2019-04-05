4120113a405ebcf13a090264bd0789801cc43b3c
SnakeJamzz <k.wittershejm@gmail.com>
2019-04-05 22:25:34 +0200

    first run of AddContactTest
    after first code improvements, deleting irrelevant parts

--- adresbook web tests/src/test/java/qa/practice1/adressbook/AddContactTest.java
+++ adresbook web tests/src/test/java/qa/practice1/adressbook/AddContactTest.java
@@ -10,14 +10,10 @@ import org.openqa.selenium.support.ui.Select;
 
 public class AddContactTest {
   private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
@@ -50,10 +46,6 @@ public class AddContactTest {
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
   }
 
   private boolean isElementPresent(By by) {
@@ -71,21 +63,6 @@ public class AddContactTest {
       return true;
     } catch (NoAlertPresentException e) {
       return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 }
